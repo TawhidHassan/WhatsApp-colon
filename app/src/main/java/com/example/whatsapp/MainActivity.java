5659f50ab1f7b814d2e91fb20ae6072878d32cf5
sifat hassan <sifat27.sh22@gmail.com>
2020-03-28 19:14:54 +0600

    add toolbar, conect with firebase

--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.whatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.main_page_toolbar_id);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Whats App");
     }
 }

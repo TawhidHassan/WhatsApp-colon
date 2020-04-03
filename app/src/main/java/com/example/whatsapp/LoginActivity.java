64ddbc28e57a7b935d3235f2ee2a6bb88f5202fa
sifat hassan <sifat27.sh22@gmail.com>
2020-04-03 19:58:17 +0600

    login function and logout function and add top potion bar

--- app/src/main/java/com/example/whatsapp/LoginActivity.java
+++ app/src/main/java/com/example/whatsapp/LoginActivity.java
@@ -1,14 +1,22 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,16 +25,27 @@ public class LoginActivity extends AppCompatActivity {
     Button loginBtn,phoneLoginByn;
     EditText userEmail,userPassword;
     TextView needNewAccount,forgetPassword;
+
+    FirebaseAuth mAuth;
+    ProgressDialog loadingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        mAuth=FirebaseAuth.getInstance();
+        cureentUser=mAuth.getCurrentUser();
         InitializeFields();
         needNewAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendUserToregisterActivity();
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowUserLogin();
             }
         });
 
@@ -40,6 +59,8 @@ public class LoginActivity extends AppCompatActivity {
         userPassword=findViewById(R.id.loginPasswordid);
         needNewAccount=findViewById(R.id.already_havenot_accout_id);
         forgetPassword=findViewById(R.id.loginForgetPasswordid);
+
+        loadingBar=new ProgressDialog(this);
     }
 
 
@@ -52,6 +73,45 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+
+
+    private void AllowUserLogin()
+    {
+        String email=userEmail.getText().toString();
+        String password=userPassword.getText().toString();
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(getApplicationContext(),"please enter your email",Toast.LENGTH_LONG).show();
+        }if (TextUtils.isEmpty(password))
+    {
+        Toast.makeText(getApplicationContext(),"please enter your password",Toast.LENGTH_LONG).show();
+    }else
+    {
+        loadingBar.setTitle("login your Account");
+        loadingBar.setMessage("please wait, whil we are login your account..");
+        loadingBar.setCanceledOnTouchOutside(true);
+        loadingBar.show();
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Intent neddNewAccountInten=new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(neddNewAccountInten);
+                            Toast.makeText(getApplicationContext(),"Successfully login",Toast.LENGTH_LONG).show();
+                            loadingBar.dismiss();
+
+                        }else {
+                            String message=task.getException().toString();
+                            Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+    }
+    }
+
     private void sendUserToLoginActivity() {
         Intent loginIntent=new Intent(LoginActivity.this,MainActivity.class);
         startActivity(loginIntent);
@@ -59,6 +119,9 @@ public class LoginActivity extends AppCompatActivity {
     private void sendUserToregisterActivity() {
         Intent neddNewAccountInten=new Intent(getApplicationContext(),RegisterActivity.class);
         startActivity(neddNewAccountInten);
+    }
+    private void sendUserToMainActivity() {
+
     }
 
 
--- app/src/main/java/com/example/whatsapp/MainActivity.java
+++ app/src/main/java/com/example/whatsapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.whatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     TabsAccessAdapter tabsAccessAdapter;
 
     FirebaseUser cureentUser;
+    FirebaseAuth mAuth;
+    ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         toolbar=findViewById(R.id.main_page_toolbar_id);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Whats App");
+
+        mAuth= FirebaseAuth.getInstance();
+        cureentUser=mAuth.getCurrentUser();
 
         viewPager=findViewById(R.id.main_viewPager_id);
         tabLayout=findViewById(R.id.main_tab_id);
@@ -51,5 +61,31 @@ public class MainActivity extends AppCompatActivity {
     private void sendUserToLoginActivity() {
         Intent loginIntent=new Intent(MainActivity.this,LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.option_menu,menu);
+         return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+         if (item.getItemId()==R.id.logOut_option_id)
+         {
+                mAuth.signOut();
+             sendUserToLoginActivity();
+         }
+        if (item.getItemId()==R.id.main_find_friend_option_id)
+        {
+
+        }
+        if (item.getItemId()==R.id.settings_option_id)
+        {
+
+        }
+        return true;
     }
 }

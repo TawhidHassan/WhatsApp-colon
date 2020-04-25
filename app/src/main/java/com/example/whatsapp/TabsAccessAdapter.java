b568fbf20ecdeaf3fce598f152241b297eaa7807
sifat hassan <sifat27.sh22@gmail.com>
2020-04-25 19:59:49 +0600

    create request fragment

--- app/src/main/java/com/example/whatsapp/TabsAccessAdapter.java
+++ app/src/main/java/com/example/whatsapp/TabsAccessAdapter.java
@@ -25,13 +25,16 @@ public class TabsAccessAdapter extends FragmentPagerAdapter {
             case 2:
                 ContactsFragment contactsFragment=new ContactsFragment();
                 return  contactsFragment;
+            case 3:
+            RequestsFragment requestsFragment=new RequestsFragment();
+                return  requestsFragment;
         }
         return null;
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return 4;
     }
 
     @Nullable
@@ -45,6 +48,8 @@ public class TabsAccessAdapter extends FragmentPagerAdapter {
                 return "Groups";
             case 2:
                 return "Contacts";
+            case 3:
+                return "Requests";
                 default:
                     return null;
         }
